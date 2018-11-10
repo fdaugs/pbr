602275945932465d24b4b133425bdd5b6c9b8cc8
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-11-10 18:25:42 +0100

    added docs for sphere

--- src/app/shapes/sphere.h
+++ src/app/shapes/sphere.h
@@ -8,14 +8,28 @@
 #include "glm/glm.hpp"
 #include "../core/ray.h"
 
+/**
+ * A class representing a sphere.
+ */
 class Sphere {
 public:
-    double rad;    // radius
-    glm::vec3 p, c;      // position, color
+    double rad;         // radius
+    glm::vec3 p, c;     // position, color
 
+    /**
+     * Create a sphere.
+     * @param rad_ Radius of the sphere
+     * @param p_ Position of the sphere
+     * @param c_ Color of the sphere
+     */
     Sphere(double rad_, glm::vec3 p_, glm::vec3 c_)
             : rad(rad_), p(p_), c(c_){};
 
+    /**
+     * Calculates an intersection with a ray.
+     * @param r Ray
+     * @return Distance parameter of the ray
+     */
     double intersect(const Ray &r) const;
 };
 
