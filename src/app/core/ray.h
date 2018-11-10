f75635511f4d91529edfa71d2d43c04e6ea7b967
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-11-10 18:25:31 +0100

    added docs for ray

--- src/app/core/ray.h
+++ src/app/core/ray.h
@@ -2,16 +2,26 @@
 // Created by fd on 04.11.2018.
 //
 
-#ifndef ASSIGNMENT1_CORE_H
-#define ASSIGNMENT1_CORE_H
+#ifndef ASSIGNMENT1_RAY_H
+#define ASSIGNMENT1_RAY_H
 
 #include "glm/glm.hpp"
 
+/**
+ * A class representing a Ray.
+ */
 struct Ray {
-    glm::vec3 o, d;
+    glm::vec3 o; // Origin
+    glm::vec3 d; // Distance
+
+    /**
+     * Create a Ray.
+     * @param o_ origin
+     * @param d_ distance
+     */
     Ray(glm::vec3 o_, glm::vec3 d_)
             : o(o_), d(d_) {}
 };
 
 
-#endif //ASSIGNMENT1_CORE_H
+#endif //ASSIGNMENT1_RAY_H
