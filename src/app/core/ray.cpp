ceb6b43d431c77d9b658f1e53a0c67d5b52d5b3b
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-11-11 20:36:32 +0100

    overridden ray stream insert operator

--- src/app/core/ray.cpp
+++ src/app/core/ray.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "ray.h"
+
+std::ostream &operator<<(std::ostream &os, const Ray &r) {
+    os << "Origin("<<r.o.x<<", "<<r.o.y<<", "<<r.o.z<<"); Direction("<<r.d.x<<", "<<r.d.y<<", "<<r.d.z<<")";
+    return os;
+}
--- src/app/core/ray.h
+++ src/app/core/ray.h
@@ -5,6 +5,7 @@
 #ifndef PBR_RAY_H
 #define PBR_RAY_H
 
+#include <ostream>
 #include "glm/glm.hpp"
 
 /**
@@ -20,7 +21,9 @@ struct Ray {
      * @param d_ distance
      */
     Ray(glm::vec3 o_, glm::vec3 d_)
-            : o(o_), d(d_) {}
+            : o(o_), d(d_) {};
+    Ray(): o(glm::vec3(0,0,0)), d(glm::vec3(0,0,-1)){};
+    friend std::ostream& operator<<(std::ostream& os, const Ray &r);
 };
 
 
